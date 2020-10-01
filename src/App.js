57142f9228fafd72a9265e2a3df341b2efd4face
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2020-10-01 22:32:30 +0700

    import other and add route to shop

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Login from './components/LoginForm';
+import Register from './components/RegisterForm';
+import Shop from './components/Shop';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
@@ -12,6 +18,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/about' component={About} />
           <Route path='/contact' component={Contact} />
+          <Route path='/shop' component={Shop} />
           <Route path='/login' component={Login} />
           <Route path='/signup' component={Register} />
         </Switch>
