5298007bb2e7d80537bf7d0a7fa0d2f065b284bb
supakit25433 <supakrit.kit@mail.kmutt.ac.th>
2020-10-01 22:17:12 +0700

    Add config of firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,11 @@
+const config = {
+    apiKey: "AIzaSyB8_yCa7J1BSLZnRKmn7wvm4S61Q-grbdA",
+    authDomain: "example-project-react.firebaseapp.com",
+    databaseURL: "https://example-project-react.firebaseio.com",
+    projectId: "example-project-react",
+    storageBucket: "example-project-react.appspot.com",
+    messagingSenderId: "728637396175",
+    appId: "1:728637396175:web:fe76f80f9ca04a030753ba"
+}
+
+export default config
